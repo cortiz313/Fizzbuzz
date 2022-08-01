9baef221f06b8ef8017d6541747a7284efdfac76
Christian Ortiz <c.ortiz313@gmail.com>
2022-08-01 13:30:07 -0400

    Restarted from Rewind base
    I brought in the code from Rewind to start from so I can skip alot of the base work.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,33 @@
-// Call Hello World
-function helloWorld() {
-alert("hello World");
+// Get the string from the page
+// Controller function
+function getValues(){
+
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+// Reverse the string
+// Logic Function
+function reverseString(userString){
+
+    let revString = "";
+
+    // reverse a string using a for loop
+    for(let i = userString.length -1; i >= 0; i--)
+        revString += userString[i];
+    return revString;
+
+}
+
+// Display the reversed string to the user
+// View Function
+function displayString(revString){
+
+    // Write to the page
+    document.getElementById("msg").innerHTML = `Your reversed string is: ${revString} `;
+
+    // Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
