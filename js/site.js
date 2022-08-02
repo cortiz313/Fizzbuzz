38e782dcc23a2b9c2753f56a06b749266ee2a20a
Christian Ortiz <c.ortiz313@gmail.com>
2022-08-01 23:23:00 -0400

    Added 2 different algorithms to solve fizzbuzz
    added an algorithm that uses booleans and switch case, and another that uses ternary operators and the truthiness of empty string to determine the fizz buzz status.

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 // Get the string from the page
 // Controller function
-function getValues(){
+function getValues() {
 
     // get the user values from the page
     let fizzValue = document.getElementById("fizz").value;
@@ -11,15 +11,13 @@ function getValues(){
     buzzValue = parseInt(buzzValue);
 
     // check that the numbers are integers
-    if(Number.isInteger(fizzValue) && Number.isInteger(buzzValue))
-    {
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+
         // call fizzbuzz
-        let fbArray = fizzBuzz(fizzValue, buzzValue);
+        let fbArray = fizzBuzzC(fizzValue, buzzValue);
         // write and display data to the screen
         displayData(fbArray);
-    }
-    else
-    {
+    } else {
         // if not integers, alert the user
         alert("You must enter integers for the fizz and buzz values.");
     }
@@ -28,23 +26,22 @@ function getValues(){
 
 // Perform the fizzbuzz operation
 // Logic Function
-function fizzBuzz(fizzValue, buzzValue){
+function fizzBuzz(fizzValue, buzzValue) {
 
     let returnArray = [];
 
-    for(let i = 1; i <= 100; i++)
-    {
+    for (let i = 1; i <= 100; i++) {
         // check if i is divisible by both fizz and buzz values first
-        if(i % fizzValue == 0 && i % buzzValue == 0)
-        returnArray.push("fizzbuzz");
+        if (i % fizzValue == 0 && i % buzzValue == 0)
+            returnArray.push("fizzbuzz");
 
         // if not, check if its divisble by the fizz value
         else if (i % fizzValue == 0)
-        returnArray.push("fizz");
+            returnArray.push("fizz");
 
         // if not, check if its divisible by the buzz value
-        else if(i % buzzValue == 0)
-        returnArray.push("buzz");
+        else if (i % buzzValue == 0)
+            returnArray.push("buzz");
 
         // if not, push the numerical value to the array
         else returnArray.push(i);
@@ -54,9 +51,57 @@ function fizzBuzz(fizzValue, buzzValue){
 
 }
 
+// A different algorithm for fizzbuzz using booleans and switch case
+function fizzBuzzB(fizzValue, buzzValue) {
+    let returnArray = [];
+    let fizz = false;
+    let buzz = false;
+
+    for (let i = 1; i < 100; i++) {
+        fizz = i % fizzValue == 0;
+        buzz = i % buzzValue == 0;
+
+        // cases based on the fizz and buzz values
+        switch (true) {
+            case fizz && buzz: {
+                returnArray.push("fizzbuzz");
+                break;
+            }
+            case fizz: {
+                returnArray.push("fizz");
+                break;
+            }
+            case buzz: {
+                returnArray.push("buzz");
+                break;
+            }
+            default:{
+                returnArray.push(i);
+                break;
+            }
+        }
+    }
+    return returnArray;
+}
+
+// A third algorithm for fizzbuzz using ternary operators and the truthiness of empty strings
+function fizzBuzzC(fizzValue, buzzValue)
+{
+    let returnArray = [];
+
+    for(let i = 1; i <= 100; i++)
+    {
+        // ternary operators to concatenate fizz or empty string and buzz or empty string. 2 empty strings will evaluate to false, and in that case value = i;
+        let value = ((i % fizzValue == 0 ? "fizz" : "") + (i % buzzValue == 0 ? "buzz" : "") || i );
+        returnArray.push(value);
+    }
+
+    return returnArray;
+}
+
 // Display fizz buzz results to user
 // View/Logic Function
-function displayData(fbArray){
+function displayData(fbArray) {
 
     // get the table body element from the page
     let tableBody = document.getElementById("results");
@@ -70,15 +115,14 @@ function displayData(fbArray){
     let number;
     let tableRow;
 
-    for(let i = 0; i < fbArray.length; i++)
-    {
+    for (let i = 0; i < fbArray.length; i++) {
 
         tableRow = document.importNode(fbTemplateRow.content, true);
 
         // grab the number of tds in the template
         let rowCols = tableRow.querySelectorAll("td");
         rowCols[0].classList.add(fbArray[i]);
-        rowCols[0].textContent = fbArray[i]; 
+        rowCols[0].textContent = fbArray[i];
 
         tableBody.appendChild(tableRow);
     }
